4e273eb1fff04212c9de0c572e9ab78218093aab
Prateek-Frisson <prateek-frisson@gmail.com>
2025-06-24 17:51:20 +0530

    conversation

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,15 +3,16 @@ import Footer from "../components/Footer";
 import Section2 from "../components/landing/Section-2/Section2";
 import Section1 from "../components/landing/Section1";
 import Section3 from "../components/landing/Section-3/Section3";
+import Conversation from "../components/conversation/Conversations";
 
 const Landing = () => {
   return (
     <div className="flex flex-col items-center min-h-screen text-center text-white bg-black max-w-screen">
+      <Conversation/>
       <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
-
       <div className="w-[80vw]">
         <Footer />
       </div>
